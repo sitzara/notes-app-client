610c345ee21bbb052f6a3885f1a05f7c2f62bdf1
sitzara <sitzara@gmail.com>
2019-11-14 17:32:30 +0300

    setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "notes-test-app-gonna-break-your-neck"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://7se794u7a3.execute-api.eu-central-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_4KnoLrXaf",
+    APP_CLIENT_ID: "3kkbkp76ub6vl90sa6u1vhk1sb",
+    IDENTITY_POOL_ID: "eu-central-1:1ecce1d9-7d87-4e75-b917-b6125dabaf43"
+  }
+};
